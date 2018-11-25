72fa791e0a41b2401d078fdd3dc3385e59db4417
wisdomtooth <cbtsao47@gmail.com>
2018-11-24 19:20:31 -0600

    add average calculator

--- tipcalculator.js
+++ tipcalculator.js
@@ -57,3 +57,10 @@ function tipCalculator(array) {
   }
   return result;
 }
+function averageTip(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
